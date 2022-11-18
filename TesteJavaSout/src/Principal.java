0d8c91a9785ea9a62671922081b6df6a3ce1324b
Ital023 <contadoitalo911@gmail.com>
2022-11-17 21:22:16 -0300

    Pedindo numero e printando

--- TesteJavaSout/src/Principal.java
+++ TesteJavaSout/src/Principal.java
@@ -1,5 +1,11 @@
+import java.util.Scanner;
+
 public class Principal {
     public static void main(String[] args) {
-        System.out.println("oi");
+        System.out.print("Insira um numero: ");
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+
+        System.out.println("O numero digitado foi " + num);
     }
 }
